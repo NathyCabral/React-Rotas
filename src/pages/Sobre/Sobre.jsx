ffc7e6e90f727d0319a8436a0ae5681e8bf56d11
Nathy Cabral <ncabral409@gmail.com>
2022-07-15 22:05:49 -0300

    Add Sobre

--- src/pages/Sobre/Sobre.jsx
+++ src/pages/Sobre/Sobre.jsx
@@ -1,14 +1,29 @@
-import Header from '../../components/Header/Header'
-import image from '../../assets/super.svg'
+import Header from "../../components/Header/Header";
+import image from "../../assets/super.svg";
+import './sobre.css'
 
 function Sobre() {
-    return(
-       <Header
-       image={image} 
-      description="ilustracao Hacker">
-        Sobre mim. 
-        </Header>
-    )
+  return (
+    <>
+      <Header image={image} description="ilustracao Hacker">
+        Sobre mim.
+      </Header>
+
+      <div>      
+        <section className="container_perfil">
+          <img
+            className="profile-img"
+            src="https://media.discordapp.net/attachments/723629983414616096/995024246474543246/eu.jpeg?width=471&height=471"
+          />
+          <div className="about">
+            <h2>Prazer, Nathy Cabral!</h2>
+            <p>Paulistana! Tatuadora e Kriptoniana nas horas vagas.</p>
+            <p>Dev Front-end</p>
+          </div>
+        </section>   
+      </div>
+    </>
+  );
 }
 
-export default Sobre
+export default Sobre;
